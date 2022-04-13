49a71d2282dbb208f7146d9975f326431dcefca3
eastpeople0301@gmail.com <sindongmin@sindongmin-ui-MacBookAir.local>
2022-04-13 22:55:57 +0900

    움직이기 및 도형 굴리기

--- js/main.js
+++ js/main.js
@@ -17,9 +17,24 @@ const BLOCKS = {
       [1, 0],
       [1, 1],
     ],
-    [],
-    [],
-    [],
+    [
+      [0, 1],
+      [1, 2],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [0, 1],
+      [1, 2],
+      [2, 1],
+      [1, 1],
+    ],
+    [
+      [2, 1],
+      [1, 2],
+      [1, 0],
+      [1, 1],
+    ],
   ],
 };
 
@@ -29,7 +44,7 @@ const movingItem = {
   type: 'tree',
   direction: 0,
   top: 0,
-  left: 3,
+  left: 0,
 };
 
 init();
@@ -54,22 +69,54 @@ function createMatrix() {
   playground.prepend(li);
 }
 
-function renderBlocks() {
+function renderBlocks(moveType) {
   const { type, direction, top, left } = tempMovingItem;
   const movingBlocks = document.querySelectorAll('.moving');
   movingBlocks.forEach(moving => {
     moving.classList.remove(type, 'moving');
   });
-  BLOCKS[type][direction].forEach(block => {
+  BLOCKS[type][direction].some(block => {
     const x = block[0] + left;
     const y = block[1] + top;
-    const target = playground.childNodes[y].childNodes[0].childNodes[x];
-    target.classList.add(type, 'moving');
+    const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, 'moving');
+    } else {
+      tempMovingItem = { ...movingItem };
+      // 재귀함수 콜스택에러 방지
+      setTimeout(() => {
+        renderBlocks();
+        if (moveType === 'top') {
+          seizeBlock();
+        }
+      }, 0);
+      return true;
+    }
   });
+  movingItem.top = top;
+  movingItem.left = left;
+  movingItem.direction = direction;
+}
+
+function seizeBlock() {
+  console.log('sieze block');
+}
+
+function checkEmpty(target) {
+  if (!target) return false;
+  return true;
 }
 
 function moveBlock(moveType, amount) {
   tempMovingItem[moveType] += amount;
+  renderBlocks(moveType);
+}
+function rotateBlock() {
+  tempMovingItem.direction += 1;
+  if (tempMovingItem.direction === 4) {
+    tempMovingItem.direction = 0;
+  }
   renderBlocks();
 }
 
@@ -79,7 +126,7 @@ document.addEventListener('keydown', e => {
     case 37:
       return moveBlock('left', -1);
     case 38:
-      return moveBlock('top', -1);
+      return rotateBlock();
     case 39:
       return moveBlock('left', 1);
     case 40:
